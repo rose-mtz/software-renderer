5ede3ef0518c15c0b0a7d351ce8bdf499eea09fb
Rosendo Martinez <rosendomartinez270@gmail.com>
2025-07-15 19:46:51 -0500

    rasterize arbatary convex polygons

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,7 @@
 #include <chrono>
 #include <cmath>
 #include <algorithm>
+#include <vector>
 #include "Window.h"
 #include "Math.h"
 
@@ -13,11 +14,16 @@ Vec4f* color_buffer = nullptr;
 
 Vec2f mouse_pos;
 
+int active_user_poly = 0;
+std::vector<std::vector<Vec2f>> list_of_user_polys (1);
+
 void resize_color_buffer();
 void clear_color_buffer(Vec4f color);
-void rasterize_point(Vec3f point, Vec4f color, int width);
+void rasterize_point(Vec2f point, Vec4f color, int width);
 void rasterize_line(Vec2f point_1, Vec2f point_2, Vec4f color, int width);
 void rasterize_trapezoid(Vec2f p1, Vec2f p2, Vec2f p3, Vec2f p4, Vec4f color);
+void rasterize_polygon(std::vector<Vec2f> polygon);
+void rasterize_triangle(Vec2f p1, Vec2f p2, Vec2f p3, Vec4f color);
 
 int main()
 {
@@ -50,21 +56,44 @@ int main()
                 mouse_pos.x = event.motion.x;
                 mouse_pos.y = event.motion.y;
             }
+            else if (event.type == SDL_EVENT_MOUSE_BUTTON_DOWN)
+            {
+                list_of_user_polys[active_user_poly].push_back(Vec2f(event.button.x, event.button.y));
+            }
+            else if (event.type == SDL_EVENT_KEY_DOWN)
+            {
+                active_user_poly++;
+                list_of_user_polys.push_back(std::vector<Vec2f>());
+            }
         }
 
-        clear_color_buffer(Vec4f(1.0f, 0.0f, 1.0f, 1.0f));
+        clear_color_buffer(Vec4f(0.0f, 0.0f, 0.0f, 1.0f));
 
-        // rasterize_line(Vec2f(window.width/2.0f,window.height/2.0f), mouse_pos, Vec4f(1.0f, 1.0f, 1.0f, 1.0f), 10);
-        // rasterize_point(Vec3f(mouse_pos.x, mouse_pos.y, 0.0f), Vec4f(1.0f, 0.0f, 0.0f, 1.0f), 5);
+        for (int i = 0; i < list_of_user_polys.size(); i++)
+        {
+            std::vector<Vec2f>& poly = list_of_user_polys[i];
 
-        float bottom_y = window.height * 0.25f;
-        Vec2f bottom_left (window.width * 0.20, bottom_y);
-        Vec2f bottom_right (window.width * 0.80, bottom_y);
-        float top_y = mouse_pos.y; // window.height * 0.75;
-        Vec2f top_left (window.width* 0.15, top_y);
-        Vec2f top_right (mouse_pos.x, top_y);
+            if (poly.size() > 2)
+            {
+                rasterize_polygon(poly);
+            }
 
-        rasterize_trapezoid(bottom_left, top_left, top_right, bottom_right, Vec4f(1.0f, 1.0f, 1.0f, 1.0f));
+            if (poly.size() > 1)
+            {
+                for (int j = 0; j < poly.size(); j++)
+                {
+                    Vec2f start = poly[j];
+                    Vec2f end = poly[(j + 1) % poly.size()];
+
+                    rasterize_line(start, end, Vec4f(1.0f, 0.0f, 0.0f, 1.0f), 2);
+                }
+            }
+
+            for (int j = 0; j < poly.size(); j++)
+            {
+                rasterize_point(poly[j], Vec4f(1.0f, 0.0f, 0.0f, 1.0f), 5);
+            }
+        }
 
         blit_window(color_buffer);
     }
@@ -86,7 +115,7 @@ void clear_color_buffer(Vec4f color)
     }
 }
 
-void rasterize_point(Vec3f point, Vec4f color, int width)
+void rasterize_point(Vec2f point, Vec4f color, int width)
 {
     // Assumes point is in device coords, and is inside bounds.
     // Ignoring depth, and AA for now
@@ -183,10 +212,106 @@ void rasterize_line(Vec2f point_1, Vec2f point_2, Vec4f color, int width)
         cur_point.y += inc_y;
 
         // March ends when past point_2
-        if ((steep_slope && cur_point.y > point_2.y) || (!steep_slope && cur_point.x > point_2.x)) // BUG?: point_2.x/y = right/top edge of screen
+        if ((steep_slope && cur_point.y > point_2.y) || (!steep_slope && cur_point.x > point_2.x)) // IDEA: BUG: point_2.x/y = right/top edge of screen
         {
             break;
         }
+    }
+}
+
+// Cuts a polygon into two pieces
+// polygon above cut line is 0
+// polygon bellow cut line is 1
+std::vector<std::vector<Vec2f>> cut_polygon(std::vector<Vec2f> polygon, float y)
+{
+    // IDEA: polygon MUST have some winding
+
+    std::vector<std::vector<Vec2f>> to_return (2);
+    std::vector<Vec2f>& top = to_return[0];
+    std::vector<Vec2f>& bottom = to_return[1];
+
+    for (int i = 0; i < polygon.size(); i++)
+    {
+        Vec2f cur = polygon[i];
+        float cur_dy = y - cur.y;
+
+        // Add current vertex to its respective list(s)
+        if (cur_dy >= 0.0f)
+        {
+            bottom.push_back(cur);
+        }
+        if (cur_dy <= 0.0f)
+        {
+            top.push_back(cur);
+        }
+
+        Vec2f next = polygon[(i + 1) % polygon.size()];
+        float next_dy = y - next.y;
+
+        // Check if we cross cut line on way to next vertex
+        if (((cur_dy < 0.0f) && (next_dy > 0.0f)) || ((cur_dy > 0.0f) && (next_dy < 0.0f)))
+        {
+            // Interpolate
+            float dx_dy = (next.x - cur.x) / (next.y - cur.y);
+            float dx = cur_dy * dx_dy;
+            Vec2f interpolated (cur.x + dx, y);
+
+            top.push_back(interpolated);
+            bottom.push_back(interpolated);
+        }
+    }
+
+    return to_return;
+}
+
+void rasterize_polygon(std::vector<Vec2f> polygon)
+{
+    // Process: cut polygon into rasterizable triangle or trapezoid
+    // IDEA: polygon is flat, and convex
+    // IDEA: polygon must have some winding
+
+    // IDEA: switch to insertion sort 
+    std::vector<float> sorted_heights (polygon.size());
+    for (int i = 0; i < sorted_heights.size(); i++) sorted_heights[i] = polygon[i].y;
+    std::sort(sorted_heights.begin(), sorted_heights.end());
+
+    std::vector<Vec2f> cur_polygon = polygon;
+    for (int i = 0; i < sorted_heights.size(); i++)
+    {
+        std::vector<std::vector<Vec2f>> split_poly = cut_polygon(cur_polygon, sorted_heights[i]);
+
+        // Rasterize bottom piece (if its triangle or trapezoid)
+        if (split_poly[1].size() == 4)
+        {
+            rasterize_trapezoid(split_poly[1][0], split_poly[1][1], split_poly[1][2], split_poly[1][3], Vec4f(1.0f, 1.0f, 1.0f, 1.0f));
+        }
+        else if (split_poly[1].size() == 3)
+        {
+            rasterize_triangle(split_poly[1][0], split_poly[1][1], split_poly[1][2], Vec4f(1.0f, 1.0f, 1.0f, 1.0f));
+        }
+
+        // Keep top piece of polygon
+        cur_polygon = split_poly[0];
+    }
+}
+
+void rasterize_triangle(Vec2f p1, Vec2f p2, Vec2f p3, Vec4f color)
+{
+    // for now I'll just pass it over to rasterize trapezoid,
+    // so triangles must have a flat top/bottom
+
+    // IDEA: I need two top points, and two bottoms points for rasterizing a trapezoid
+    if (p1.y == p2.y)
+    {
+        rasterize_trapezoid(p1, p2, p3, p3, color);
+    }
+    else if (p1.y == p3.y)
+    {
+        rasterize_trapezoid(p1, p2, p3, p2, color);
+    }
+    else
+    {
+        rasterize_trapezoid(p1, p2, p3, p1, color);
     }
 }
 
@@ -201,7 +326,9 @@ void rasterize_trapezoid(Vec2f p1, Vec2f p2, Vec2f p3, Vec2f p4, Vec4f color)
     if (p1.y > p2.y) std::swap(p1,p2);
     if (p2.y > p3.y) std::swap(p2,p3);
     if (p3.y > p4.y) std::swap(p3,p4);
+    if (p1.y > p2.y) std::swap(p1,p2);
     if (p2.y > p3.y) std::swap(p2,p3);
+    if (p1.y > p2.y) std::swap(p1,p2);
 
     // Sort by x
     if (p1.x > p2.x) std::swap(p1,p2);
