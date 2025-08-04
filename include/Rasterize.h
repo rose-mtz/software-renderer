ddfd45f3a465c6cc14e1e7f90ffec82981aa2882
Rosendo Martinez <rosendomartinez270@gmail.com>
2025-08-04 18:38:43 -0500

    notes

--- include/Rasterize.h
+++ include/Rasterize.h
@@ -7,3 +7,8 @@
 void rasterize_point(const Vertex& v, int width, Buffer* buffer);
 void rasterize_line(const Vertex& v0, const Vertex& v1, int width, Buffer* buffer);
 void rasterize_polygon(const std::vector<Vertex>& vertices, Buffer* buffer);
+
+// NOTE: currently, rasterizer expect in-bounds device coordinates
+//          if they are out-of-bounds, and the primitive to render
+//          is large, then it will still try to rasterizer every pixel it covers
+//          this can really slow down performance, halt it even
